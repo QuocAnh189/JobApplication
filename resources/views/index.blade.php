f4880ee5b1560cb9d090e4a8b5f2c9c9d7c207c9
QuocAnh189 <anhquoc18092003@gmail.com>
2024-10-20 10:45:44 +0700

    setup tailwind for project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="ie=edge" http-equiv="X-UA-Compatible">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="flex w-full justify-center text-2xl text-red-500">Hello world</h1>
+</body>
+
+</html>
